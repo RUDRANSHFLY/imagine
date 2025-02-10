3e4cc731d0c8b53d5eaf4a12fa0a74883be7ef53
RUDRANSHFLY <mayamayavir@gmail.com>
2025-02-10 15:15:14 +0530

    Add EmptySearch component to display a message when no search results are found

---
+++ src/app/(dashboard)/_components/empty-search.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+
+const EmptySearch = () => {
+  return (
+    <div className={'h-full flex flex-col items-center justify-center'}>
+      <Image src={'/icons/empty-search.webp'} width={140} height={140} alt={'Empty'} />
+      <h2 className={'text-2xl font-semibold mt-6'}>
+        No results found!
+      </h2>
+      <p className={'text-muted-foreground text-sm mt-2'}>
+        Try searching for something else.
+      </p>
+    </div>
+  )
+}
+
+export default EmptySearch
