83b50ab54408a1506fb22d1faa6103a4f6997ebe
RUDRANSHFLY <mayamayavir@gmail.com>
2025-03-02 14:43:29 +0530

    Refactor button click handling in Footer component to prevent default and stop propagation

--- src/app/(dashboard)/_components/board-card/footer.tsx
+++ src/app/(dashboard)/_components/board-card/footer.tsx
@@ -20,14 +20,23 @@ const Footer = ({
   onClick,
   title
 }: FooterProps) => {
-    
+
+
+  const handleClick = (event : React.MouseEvent<HTMLButtonElement,MouseEvent>) => {
+    event.preventDefault();
+    event.stopPropagation();
+    onClick();
+  }
+  
+
+
   return (
     <div className={"relative bg-white p-3"}>
       <p className={"text-[13px] truncate max-w-[calc(100%-20px)]"}>{title}</p>
       <p className={"opacity-0 group-hover:opacity-100 transition-opacity text-[11px] text-muted-foreground truncate"}>
         {authorLabel} , {createdAtLabel}
       </p>
-      <button title={title} type={"button"} disabled={disabled} onClick={onClick} className={cn("opacity-0 group-hover:opacity-100 transition absolute top-3 right-3 text-muted-foreground hover:text-blue-600",disabled && 
+      <button title={title} type={"button"} disabled={disabled} onClick={handleClick} className={cn("opacity-0 group-hover:opacity-100 transition absolute top-3 right-3 text-muted-foreground hover:text-blue-600",disabled && 
         "cursor-not-allowed opacity-75"
       )}>
         <Star className={cn("h-4 w-4",isFavorite && "fill-blue-600 text-blue-600")} />
