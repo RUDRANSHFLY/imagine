32d26d8023396d871f2078ea9e722e5f56e8dfe8
RUDRANSHFLY <mayamayavir@gmail.com>
2025-03-02 14:43:08 +0530

    Update BoardList component to display loading state and favorite status for boards

--- src/app/(dashboard)/_components/board-list.tsx
+++ src/app/(dashboard)/_components/board-list.tsx
@@ -22,7 +22,24 @@ const BoardList = ({ orgId, query }: BoardListProps) => {
   const data = useQuery(api.boards.get, { orgId: orgId });
 
   if (data === undefined) {
-    return <div>Loading....</div>;
+    return (
+      <div>
+        <h2 className={"text-3xl"}>
+          {query.favorites ? "Favorite Boards" : "Team Boards"}
+        </h2>
+        <div
+          className={
+            "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10 "
+          }
+        >
+          <NewBoardButton orgId={orgId} disabled />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+        </div>  
+      </div>
+    );
   }
 
   if (!data?.length && query.search) {
@@ -42,21 +59,25 @@ const BoardList = ({ orgId, query }: BoardListProps) => {
       <h2 className={"text-3xl"}>
         {query.favorites ? "Favorite Boards" : "Team Boards"}
       </h2>
-      <div className={"grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10 "}>
-          <NewBoardButton orgId={orgId} />
-          {data.map((board) => (
-            <BoardCard 
-              key={board._id} 
-              id={board._id} 
-              title={board.title} 
-              authorName={board.authorName} 
-              authorId={board.authorId} 
-              createdAt={board._creationTime} 
-              imageUrl={board.imageUrl} 
-              orgId={board.orgId} 
-              isFavorite={false} 
-            />
-          ))}
+      <div
+        className={
+          "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10 "
+        }
+      >
+        <NewBoardButton orgId={orgId} />
+        {data.map((board) => (
+          <BoardCard
+            key={board._id}
+            id={board._id}
+            title={board.title}
+            authorName={board.authorName}
+            authorId={board.authorId}
+            createdAt={board._creationTime}
+            imageUrl={board.imageUrl}
+            orgId={board.orgId}
+            isFavorite={board.isFavorite}
+          />
+        ))}
       </div>
     </div>
   );
