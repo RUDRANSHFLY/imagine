692196a17d9c1ad7953c57faad3d4760b20606d2
RUDRANSHFLY <mayamayavir@gmail.com>
2025-03-02 14:42:37 +0530

    Enhance get query to include favorite status for boards

--- convex/boards.ts
+++ convex/boards.ts
@@ -1,23 +1,41 @@
-import {v} from "convex/values";
+import { v } from "convex/values";
 import { query } from "./_generated/server";
 
 export const get = query({
-    args : {
-        orgId : v.string()
+    args: {
+        orgId: v.string()
     },
-    handler : async (ctx , args) => {
+    handler: async (ctx, args) => {
         const identity = await ctx.auth.getUserIdentity();
 
-        if(!identity){
+        if (!identity) {
             throw new Error("Unauthorized");
         }
 
         const boards = await ctx.db
             .query("boards")
-            .withIndex("by_org",(q) => q.eq("orgId",args.orgId))
+            .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
             .order("desc")
             .collect();
-            
-        return boards;
+
+
+        const boardsWithFavoriteRelation = boards.map(async (board) => {
+            const favorite = await ctx.db
+                .query("userFavorites")
+                .withIndex("by_user_board", (q) => q.eq(
+                    "userId", identity.subject
+                ).eq("boardId", board._id))
+
+                .unique();
+            return {
+                ...board,
+                isFavorite: !!favorite
+            };
+        });
+
+        const boardsWithFavoriteBoolean = Promise.all(boardsWithFavoriteRelation);
+
+
+        return boardsWithFavoriteBoolean;
     }
 })
