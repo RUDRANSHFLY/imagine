e6c2da189809ce45475a02f85b92096ff28671e5
RUDRANSHFLY <mayamayavir@gmail.com>
2025-03-02 08:26:44 +0530

    Add NewBoardButton component for creating new boards

---
+++ src/app/(dashboard)/_components/new-board-button.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+
+import React from 'react'
+import { cn } from "@/lib/utils";
+import { Plus } from 'lucide-react';
+import { useApiMutation } from '@/hooks/use-api-mutation';
+import { api } from '../../../../convex/_generated/api';
+
+
+
+interface NewBoardButtonProps{
+    orgId : string,
+    disabled? :boolean,
+}
+
+const NewBoardButton = ({orgId,disabled} : NewBoardButtonProps) => {
+
+    const {mutate,pending} = useApiMutation(api.board.create)
+
+    const onClick = () => {
+        mutate({
+            orgId,
+            title : "New Board"
+        })
+    }
+
+  return (
+    <button 
+        title={orgId} 
+        type={"button"} 
+        disabled={disabled || pending} 
+        onClick={onClick} 
+        className={cn("col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col justify-center items-center py-6", (disabled || pending) && "opacity-75")}
+    >
+      <div />
+      <Plus className={'h-12 w-12 text-white stroke-1'} />
+      <p className={'text-xs text-white font-light'}>
+        New board
+      </p>
+    </button>
+  )
+}
+  
+export default NewBoardButton
