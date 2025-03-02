7c10c2354fad8022626890ccc61aafb374e617a8
RUDRANSHFLY <mayamayavir@gmail.com>
2025-03-02 14:43:49 +0530

    Add favorite toggle functionality and loading state to BoardCard component

--- src/app/(dashboard)/_components/board-card/index.tsx
+++ src/app/(dashboard)/_components/board-card/index.tsx
@@ -6,11 +6,17 @@ import Image from 'next/image';
 import Overlay from './overlay';
 
 
+
 import { useAuth } from '@clerk/nextjs';
 import {formatDistanceToNow} from "date-fns"
 import Footer from './footer';
 
-
+import { Skeleton } from '@/components/ui/skeleton';
+import { Actions } from '@/components/action';
+import { MoreHorizontal } from 'lucide-react';
+import { useApiMutation } from '@/hooks/use-api-mutation';
+import { api } from '../../../../../convex/_generated/api';
+import { toast } from 'sonner';
 
 interface BoardCardProps {
   id : string;
@@ -29,7 +35,27 @@ const BoardCard = ({authorId,authorName,createdAt,id,imageUrl,isFavorite,orgId,t
   const {userId} = useAuth();
   const authorLabel = userId === authorId ? "You" : authorName;
   const createdAtLabel = formatDistanceToNow(createdAt,{addSuffix : true})
+  const {
+    mutate : onFavorite,
+    pending : pendingFavorite
+  } = useApiMutation(api.board.favorite)
+  
+  const {
+    mutate : onUnFavorite,
+    pending : pendingUnfavorite
+  } = useApiMutation(api.board.unfavorite)
 
+ 
+  const toggleFavorite = () => {
+    if(isFavorite){
+      onUnFavorite({id})
+      .catch(() => toast.error('Failed to remove favorite'))
+    }else{
+      onFavorite({id,orgId})
+      .catch(() => toast.error("Failed to add favorite"))
+    }
+  }
+ 
   return (
     <Link href={`/board/${id}/${orgId}`}>
     <div className={'group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden'}>
@@ -41,11 +67,33 @@ const BoardCard = ({authorId,authorName,createdAt,id,imageUrl,isFavorite,orgId,t
             className={'object-fit'}
           />
           <Overlay />
+          <Actions id={id} title={title} side={"right"}>
+              <button title='actions' type='button' className='absolute top-1 right-1 opacity-0 group-hover:opacity-100 px-3 py-2 outline-none'>
+                <MoreHorizontal className='text-white opacity-75 hover:opacity-100 transition-opacity' />
+              </button>
+           </Actions>
       </div>
-      <Footer  title={title} authorLabel={authorLabel} createdAtLabel={createdAtLabel} isFavorite={isFavorite} onClick={() => {}} disabled={false} />
+      <Footer  
+        title={title} 
+        authorLabel={authorLabel} 
+        createdAtLabel={createdAtLabel} 
+        isFavorite={isFavorite} 
+        onClick={toggleFavorite} 
+        disabled={pendingFavorite || pendingUnfavorite} 
+      />
     </div>
     </Link>
   )
 }
 
 export default BoardCard;
+
+
+
+BoardCard.Skeleton = function BoardCardSkeleton(){
+  return (
+    <div className={'aspect-[100/127] rounded-lg flex flex-col justify-between '}>
+      <Skeleton className='h-full w-full' />
+    </div>
+  )
+}
