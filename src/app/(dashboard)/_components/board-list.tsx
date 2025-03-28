336f6b0956bd1e619d7b2cbd426569d1919cbef5
RUDRANSHFLY <mayamayavir@gmail.com>
2025-03-28 09:09:21 +0530

    Update BoardList to include query parameters in API call

--- src/app/(dashboard)/_components/board-list.tsx
+++ src/app/(dashboard)/_components/board-list.tsx
@@ -19,7 +19,7 @@ interface BoardListProps {
 }
 
 const BoardList = ({ orgId, query }: BoardListProps) => {
-  const data = useQuery(api.boards.get, { orgId: orgId });
+  const data = useQuery(api.boards.get, { orgId: orgId , ...query});
 
   if (data === undefined) {
     return (
