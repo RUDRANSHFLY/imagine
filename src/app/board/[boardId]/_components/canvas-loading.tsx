eea9d2c5183a9b07bdf2cc96baa3351e514ab8fc
RUDRANSHFLY <mayamayavir@gmail.com>
2025-03-29 09:24:08 +0530

    Skeleton for Info Part and ToolBar Add

--- src/app/board/[boardId]/_components/canvas-loading.tsx
+++ src/app/board/[boardId]/_components/canvas-loading.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Loader } from 'lucide-react'
+import { InfoSkeleton } from './info'
+import { ParticipantsSkeleton } from './participants'
+import { ToolBarSkeleton } from './toolbar'
 
 
 const CanvasLoading = () => {
@@ -7,6 +10,9 @@ const CanvasLoading = () => {
   return (
     <main className='h-full w-full relative bg-neutral-100 touch-none flex items-center justify-center'>
       <Loader className='w-6 h-6 text-muted-foreground animate-spin'/>
+      <InfoSkeleton />
+      <ParticipantsSkeleton />
+      <ToolBarSkeleton />
     </main>
   )
 }
