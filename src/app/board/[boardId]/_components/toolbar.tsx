98f699ec4e0e5ca769a8dd99a22012f6dec25dfb
RUDRANSHFLY <mayamayavir@gmail.com>
2025-03-29 09:27:40 +0530

    Refactor ToolBar component layout and add ToolBarSkeleton for loading state

--- src/app/board/[boardId]/_components/toolbar.tsx
+++ src/app/board/[boardId]/_components/toolbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ToolBar = () => {
   return (
-    <div className='absolute top-[50%] -translate-y-[50%] flex flex-col gap-y-4'>
+    <div className='absolute top-[50%] left-2 -translate-y-[50%] flex flex-col gap-y-4'>
       <div className='bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md'>
         <div>
             Pencil
@@ -26,6 +26,12 @@ const ToolBar = () => {
         </div>
       </div>
     </div>
+  )
+}
+
+export function ToolBarSkeleton(){
+  return (
+    <div className='absolute top-[50%] left-2 -translate-y-[50%] flex flex-col gap-y-4 bg-white h-[360px] w-[52px] shadow-md rounded-md'/>
   )
 }
 
