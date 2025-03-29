f7199730609075d4bd13a9814e5c8d713d0c435f
RUDRANSHFLY <mayamayavir@gmail.com>
2025-03-29 09:23:41 +0530

    single board get Function Add

--- convex/board.ts
+++ convex/board.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values"
-import { mutation } from "./_generated/server"
+import { mutation, query } from "./_generated/server"
 
 
 const images = [
@@ -200,4 +200,14 @@ export const unfavorite = mutation({
 
 
     },
+})
+
+
+export const get = query({
+    args : {id  : v.id("boards")},
+    handler : async (ctx,args) => {
+        const board = ctx.db.get(args.id);
+
+        return board;
+    }
 })
