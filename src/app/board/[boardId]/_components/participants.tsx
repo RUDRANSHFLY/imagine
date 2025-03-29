ef5b170f095fc59127a3c5d93569d3c58bc8df6e
RUDRANSHFLY <mayamayavir@gmail.com>
2025-03-29 09:27:51 +0530

    Add ParticipantsSkeleton component for loading state in participants list

--- src/app/board/[boardId]/_components/participants.tsx
+++ src/app/board/[boardId]/_components/participants.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { Skeleton } from "@/components/ui/skeleton";
+import React from "react";
 
 const Participants = () => {
   return (
-    <div className='absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md'>
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
       List Of USer
     </div>
-  )
-}
+  );
+};
 
-export default Participants
+export function ParticipantsSkeleton() {
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]">
+      <Skeleton className="h-full w-full bg-muted"/>
+    </div>
+  );
+};
+
+export default Participants;
