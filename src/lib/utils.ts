3cd208bdd7bc789b90e3cc1270a9bad9f8eec55a
RUDRANSHFLY <mayamayavir@gmail.com>
2025-03-30 16:19:53 +0530

    Add color mapping and pointer event utility functions to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,39 @@
+import { Camera } from "@/types/canvas";
 import { clsx, type ClassValue } from "clsx"
+import React from "react";
 import { twMerge } from "tailwind-merge"
+
+const COLORS = [
+  "#F1EFEC",
+  "#D4C9BE",
+  "#123458",
+  "#030303",
+  "#F7374F",
+  "#88304E",
+  "#522546",
+  "#2C2C2C",
+  "#604652",
+  "#735557",
+  "#97866A",
+  "#D29F80",
+  "#F2EFE7",
+  "#9ACBD0",
+  "#48A6A7",
+  "#006A71",
+]
+
+export function connectionIdToColor(connectionId : number) : string {
+  return COLORS[connectionId % COLORS.length];
+}
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+export function pointerEventToCanvasPoint(e : React.PointerEvent,camera : Camera){
+  return {
+    x : Math.round(e.clientX - camera.x),
+    y : Math.round(e.clientY - camera.y),
+  }
 }
