3cdc871723114eb36d1faf79faf672b4f195a45a
RUDRANSHFLY <mayamayavir@gmail.com>
2025-03-30 16:21:24 +0530

    Add UserAvatar component for displaying user avatars with fallback support

---
+++ src/app/board/[boardId]/_components/user-avatar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Hint from '@/components/hint'
+import { Avatar , AvatarFallback , AvatarImage } from '@/components/ui/avatar'
+
+
+interface UserAvatarProps{
+    src?: string ;
+    name? : string ;
+    fallback? : string;
+    borderColor?:string ;
+}
+
+const UserAvatar = ({borderColor,fallback,name,src} : UserAvatarProps) => {
+  return (
+    <Hint label={name || "Teammate"} side={"bottom"} sideOffset={18}>
+        <Avatar className='w-8 h-8 border-2' style={{borderColor}}>
+            <AvatarImage src={src} />
+            <AvatarFallback className='text-xs font-semibold'>
+                {fallback || name?.charAt(0)?.toUpperCase()}
+            </AvatarFallback>
+        </Avatar>
+    </Hint>
+  )
+}p
+
+export default UserAvatar
