3221d1edd2475955c5ce5446b07a96b80ed2d42b
RUDRANSHFLY <mayamayavir@gmail.com>
2025-04-03 16:15:15 +0530

    Enhance Room component with initial storage for layers and selection state

--- src/components/room.tsx
+++ src/components/room.tsx
@@ -1,25 +1,39 @@
-"use client"
+"use client";
 
-import { ReactNode } from "react"
-import {LiveblocksProvider , RoomProvider , ClientSideSuspense} from "@liveblocks/react/suspense"
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense
+} from "@liveblocks/react/suspense";
+import { LiveMap, LiveObject, LiveList } from "@liveblocks/client";
 
 
-interface RoomProps{
-    children : ReactNode,
-    roomId : string,
-    fallback : NonNullable<ReactNode> | null
+import { Layer } from "@/types/canvas";
+
+interface RoomProps {
+  children: ReactNode;
+  roomId: string;
+  fallback: NonNullable<ReactNode> | null;
 }
 
-const Room = ({children , roomId , fallback}:RoomProps) => {
+const Room = ({ children, roomId, fallback }: RoomProps) => {
   return (
-    <LiveblocksProvider throttle={16} authEndpoint={'/api/liveblocks-auth'}>
-      <RoomProvider id={roomId} initialPresence={{cursor : null}}>
+    <LiveblocksProvider throttle={16} authEndpoint={"/api/liveblocks-auth"}>
+      <RoomProvider
+        id={roomId}
+        initialPresence={{ cursor: null ,selection : [] }}
+        initialStorage={{
+          layers: new LiveMap<string, LiveObject<Layer>>(),
+          layerIds: new LiveList([])
+        }}
+      >
         <ClientSideSuspense fallback={fallback}>
-            {() => children}
+          {() => children}
         </ClientSideSuspense>
       </RoomProvider>
     </LiveblocksProvider>
-  )
-}
+  );
+};
 
-export default Room
+export default Room;
