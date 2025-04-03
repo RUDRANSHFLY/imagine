524f3b5b959cac0e367d05452df8842f5f3ef079
RUDRANSHFLY <mayamayavir@gmail.com>
2025-04-03 16:02:58 +0530

    Add color utility function and refine pointer event calculations

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { Camera } from "@/types/canvas";
+import { Camera, Color } from "@/types/canvas";
 import { clsx, type ClassValue } from "clsx"
 import React from "react";
 import { twMerge } from "tailwind-merge"
@@ -33,7 +33,12 @@ export function cn(...inputs: ClassValue[]) {
 
 export function pointerEventToCanvasPoint(e : React.PointerEvent,camera : Camera){
   return {
-    x : Math.round(e.clientX - camera.x),
-    y : Math.round(e.clientY - camera.y),
+    x : Math.round(e.clientX) - camera.x,
+    y : Math.round(e.clientY) - camera.y,
   }
+}
+
+
+export function colorToCss(color : Color){
+  return `#${color.r.toString(16).padStart(2,"0")}${color.g.toString(16).padStart(2,"0")}${color.b.toString(16).padStart(2,"0")}`
 }
