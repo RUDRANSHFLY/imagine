4e1ea45d13a4bf6634aef722d06aa226d7f189ac
RUDRANSHFLY <mayamayavir@gmail.com>
2025-04-11 18:55:58 +0530

    Color Combo Function Add

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -109,3 +109,10 @@ export function findIntersectingLayersWithReactangle (
 
   return ids;
 }
+
+
+export function getContrastingTextColor(color : Color){
+  const luminance  = 0.299 * color.r + 0.587 * color.g + 0.114 * color.b;
+
+  return luminance > 182 ? "black" : "white";
+}
