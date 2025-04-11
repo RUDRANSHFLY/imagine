baaa91f70a6cc0a3091848eec9bfa9e2dc61101c
RUDRANSHFLY <mayamayavir@gmail.com>
2025-04-11 18:35:50 +0530

    Extra Layer Case Add

--- src/app/board/[boardId]/_components/layer-preview.tsx
+++ src/app/board/[boardId]/_components/layer-preview.tsx
@@ -1,49 +1,66 @@
-"use client"
+"use client";
 
-import { LayerType } from '@/types/canvas';
-import { useStorage } from '@liveblocks/react/suspense';
-import React from 'react'
-import { memo } from 'react';
-import Rectangle from './rectangle';
+import { LayerType } from "@/types/canvas";
+import { useStorage } from "@liveblocks/react/suspense";
+import React from "react";
+import { memo } from "react";
+import Rectangle from "./rectangle";
+import Circle from "./circle";
+import Text from "./text";
 
 interface LayerPreviewProps {
-  id : string ;
-  onLayerPointDown : (e : React.PointerEvent,layerId : string) => void ;
-  selectionColor : string;
+  id: string;
+  onLayerPointDown: (e: React.PointerEvent, layerId: string) => void;
+  selectionColor: string;
 }
 
-const LayerPreview = memo(({id,onLayerPointDown,selectionColor} : LayerPreviewProps) => {
-  
-  const layer = useStorage((root) => root.layers.get(id))
+const LayerPreview = memo(
+  ({ id, onLayerPointDown, selectionColor }: LayerPreviewProps) => {
+    const layer = useStorage((root) => root.layers.get(id));
 
-  if(!layer){
-    return null
-  }
-
-
-  switch(layer.type){
-    case LayerType.Reactangle:
-      return (
-        <Rectangle 
-          id={id}
-          layer={layer}
-          onPointerDown={onLayerPointDown}
-          selectionColor={selectionColor}
-        />
-      )
-
-    default:
-      console.warn("Unknown Layer type")
+    if (!layer) {
       return null;
+    }
+
+    switch (layer.type) {
+      case LayerType.Text:
+        return (
+          <Text
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointDown}
+            selectionColor={selectionColor}
+          />
+        );
+      case LayerType.Circle:
+        return (
+          <Circle
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointDown}
+            selectionColor={selectionColor}
+          />
+        );
+
+      case LayerType.Reactangle:
+        return (
+          <Rectangle
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointDown}
+            selectionColor={selectionColor}
+          />
+        );
+
+      default:
+        console.warn("Unknown Layer type");
+        return null;
+    }
+
+    return <div></div>;
   }
-  
-  return (
-    <div>
-        
-    </div>
-  )
-})
+);
 
-LayerPreview.displayName = "LayerPreview"
+LayerPreview.displayName = "LayerPreview";
 
-export default LayerPreview
+export default LayerPreview;
