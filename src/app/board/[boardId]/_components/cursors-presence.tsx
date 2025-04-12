934ebc904aa05ae785d94e3c81f8a922757184ea
RUDRANSHFLY <mayamayavir@gmail.com>
2025-04-12 18:52:36 +0530

    Refactor CursorPresence component to include Drafts for rendering pencil drafts and colors

--- src/app/board/[boardId]/_components/cursors-presence.tsx
+++ src/app/board/[boardId]/_components/cursors-presence.tsx
@@ -2,19 +2,52 @@
 
 import React from "react";
 import { memo } from "react";
-import { useOthersConnectionIds } from "@liveblocks/react/suspense";
+import {
+  shallow,
+  useOthersConnectionIds,
+  useOthersMapped,
+} from "@liveblocks/react/suspense";
 import Cursor from "./cursor";
+import Path from "./path";
+import { colorToCss } from "@/lib/utils";
 
 const Cursors = () => {
   const ids = useOthersConnectionIds();
 
+  return (
+    <>
+      {ids.map((connectionId) => (
+        <Cursor connectionId={connectionId} key={connectionId} />
+      ))}
+    </>
+  );
+};
 
+const Drafts = () => {
+  const others = useOthersMapped(
+    (other) => ({
+      penCilDraft: other.presence.pencilDraft,
+      penColor: other.presence.penColor,
+    }),
+    shallow
+  );
 
   return (
     <>
-      {ids.map((connectionId) =>  (
-        <Cursor connectionId={connectionId} key={connectionId} />
-      ))}
+      {others.map(([key, other]) => {
+        if (other.penCilDraft) {
+          return (
+            <Path
+              key={key}
+              x={0}
+              y={0}
+              points={other.penCilDraft ?? []}
+              fill={other.penColor ? colorToCss(other.penColor) : "#000"}
+            />
+          );
+        }
+        return null;
+      })}
     </>
   );
 };
@@ -22,6 +55,7 @@ const Cursors = () => {
 const CursorPresence = memo(() => {
   return (
     <>
+      <Drafts />
       <Cursors />
     </>
   );
