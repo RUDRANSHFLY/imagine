f13fd432cae5dbade8b6a610a9005e84dc602d2f
RUDRANSHFLY <mayamayavir@gmail.com>
2025-04-12 17:46:41 +0530

    Add pencilDraft and penColor to initialPresence in Room component

--- src/components/room.tsx
+++ src/components/room.tsx
@@ -22,7 +22,7 @@ const Room = ({ children, roomId, fallback }: RoomProps) => {
     <LiveblocksProvider throttle={16} authEndpoint={"/api/liveblocks-auth"}>
       <RoomProvider
         id={roomId}
-        initialPresence={{ cursor: null ,selection : [] }}
+        initialPresence={{ cursor: null ,selection : [] , pencilDraft : null , penColor : null}}
         initialStorage={{
           layers: new LiveMap<string, LiveObject<Layer>>(),
           layerIds: new LiveList([])
