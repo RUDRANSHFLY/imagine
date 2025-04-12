ce27c6f12a700d646b1e20f4bf6d160851df47bb
RUDRANSHFLY <mayamayavir@gmail.com>
2025-04-12 17:51:17 +0530

    Add action module to API utility for improved function referencing

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,6 +13,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as action from "../action.js";
 import type * as board from "../board.js";
 import type * as boards from "../boards.js";
 
@@ -25,6 +26,7 @@ import type * as boards from "../boards.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  action: typeof action;
   board: typeof board;
   boards: typeof boards;
 }>;
